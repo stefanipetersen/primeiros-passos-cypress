d71a36e1efd99cc21bdbf810b82b004c91c18833
stefanipetersen <stefanipetersen2604@gmail.com>
2023-12-26 13:37:13 -0300

    First commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,21 @@
+describe('Orange HRM Tests', () => {
+  it('Success Login', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+
+  })
+
+  it('Fail Login', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Adminn')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin1233')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert')
+    
+  })
+
+})
